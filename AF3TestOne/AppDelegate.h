329b1417465dce95c982a53f9c639c7662f70fba
Jafar <jiayuanfa@yeah.net>
2017-09-13 10:27:00 +0800

    Initial Commit

---
+++ AF3TestOne/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AF3TestOne
+//
+//  Created by JiaYuanFa on 2017/9/13.
+//  Copyright © 2017年 JiaYuanFa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AF3TestOne/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  AF3TestOne
+//
+//  Created by JiaYuanFa on 2017/9/13.
+//  Copyright © 2017年 JiaYuanFa. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
